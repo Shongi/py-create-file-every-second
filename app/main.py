dc7d56903cc0d930326d690884b51e160b80ee0a
Shongi <shongi85@gmail.com>
2026-05-13 15:14:34 +0200

    fix linting errors

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 from time import sleep
 
 
-def main():
+def main() -> None:
     while True:
         now = datetime.now()
-        formatted_now = now.strftime('%Y-%m-%d %H:%M:%S')
+        formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
         filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
         with open(filename, "w") as f:
             f.write(formatted_now)
