14935b055a314a1f31b5e568982ddd226e8c0d32
Shongi <shongi85@gmail.com>
2026-05-13 14:59:50 +0200

    file every second

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from time import sleep
 
 
 def main():
-    # write your code here
-    pass
+    while True:
+        now = datetime.now()
+        formatted_now = now.strftime('%Y-%m-%d %H:%M:%S')
+        filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
+        with open(filename, "w") as f:
+            f.write(formatted_now)
+        print(f"{formatted_now} {filename}")
+        sleep(1)
 
 
 if __name__ == "__main__":
